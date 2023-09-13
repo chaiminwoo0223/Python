ae3d61905107e69f021e40dcd9d421f54eea4b05
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-09-13 09:24:31 +0900

    Add files via upload

---
+++ 02/operator1.py
@@ -0,0 +1,17 @@
+# 숫자 복합 대입 연산자 1
+number = 100
+
+number += 10
+print("number +=", number)
+
+number -= 10
+print("number -=", number)
+
+number *= 10
+print("number *=", number)
+
+number /= 10
+print("number /=", number)
+
+number %= 10
+print("number %=", number)
---
+++ 02/variable1.py
@@ -0,0 +1,9 @@
+# pi
+pi = 3.14159265
+
+print(pi + 2)
+print(pi - 2)
+print(pi * 2)
+print(pi / 2)
+print(pi // 2)
+print(pi % 2)
