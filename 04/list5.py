b30d0566b9b97aaa314f9e5bbbd7f862b7aab3c1
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-10-04 16:04:21 +0900

    Add files via upload

---
+++ 04/list5.py
@@ -0,0 +1,3 @@
+arr = [273, 32, 103, "문자열", True, False]
+arr[0] = "변경"
+print(arr)
---
+++ 04/list8.py
@@ -0,0 +1,6 @@
+# 리스트 정렬하기
+arr = [52, 273, 103, 32, 275, 1, 7]
+arr.sort() # 오름차순 정렬
+print(arr)
+arr.sort(reverse=True) # 내림차순 정렬
+print(arr)
