9bff8c0f8bf2e47e2202fe5d937e16ef99109a1f
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-10-04 18:51:04 +0900

    Add files via upload

---
+++ 04/for1.py
@@ -0,0 +1,10 @@
+arr = [273, 32, 103, 57, 52] # 리스트
+hello = "안녕하세요" # 문자열
+
+# for 반복문과 리스트
+for e in arr:
+    print(e)
+
+# for 반복문과 문자열
+for e in hello:
+    print(e)
---
+++ 04/for3.py
@@ -0,0 +1,9 @@
+# 전개 연산자: *
+a = [1, 2, 3, 4]
+b = [*a]
+c = [*a, 5]
+
+print(a)
+print(b)
+print(c)
+print(*c)
