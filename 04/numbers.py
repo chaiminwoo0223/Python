0af37b5be6e6e348ca4beeee6caf8f597b8bd997
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-10-15 17:50:23 +0900

    Add files via upload

--- 04/numbers.py
+++ 04/numbers.py
@@ -1,19 +1,17 @@
 # 숫자의 종류
 numbers = [1, 2, 3, 4, 1, 2, 3, 1, 4, 1, 2, 3]
 counter = {}
-count = 0
 
 for number in numbers:
     if number in counter:
         counter[number] += 1
     else:
         counter[number] = 1
-        count += 1
 
 print(
 """
 {}에서 
 사용된 숫자의 종류는 {}개 입니다.
 참고: {}
-""".format(numbers, count, counter)
+""".format(numbers, len(counter), counter)
 )
