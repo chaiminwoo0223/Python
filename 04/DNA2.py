db412333500e4e28c9ac1fc5a0c675fbc131d780
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-10-16 12:29:51 +0900

    Add files via upload

--- 04/DNA2.py
+++ 04/DNA2.py
@@ -1,3 +1,12 @@
 # 염기 코돈 개수
 dna = input("염기 서열을 입력해주세요: ")
 counter = {}
+
+for i in range(0, len(dna), 3):
+    codon = dna[i:i+3]
+    if codon in counter:
+        counter[codon] += 1
+    else:
+        counter[codon] = 1
+
+print(counter)
