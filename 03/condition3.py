6ca66dced72368db50e9ddd70df4bb5b94120cde
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2023-11-25 13:40:17 +0900

    Delete 03 directory

--- 03/condition3.py
+++
@@ -1,8 +0,0 @@
-# 입력
-number = int(input("정수 입력> "))
-
-# 짝수/홀수 구분
-if number % 2 == 0:
-    print("짝수")
-else:
-    print("홀수")
--- 03/pass_keyword.py
+++
@@ -1,9 +0,0 @@
-# pass
-number = int(input("정수 입력> "))
-
-if number > 0:
-    pass
-elif number < 0:
-    pass
-else:
-    pass
